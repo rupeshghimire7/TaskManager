24c36290935d11a65b3f06d4a1536210d591242f
SandeshGC <gcsandesh01@gmail.com>
2024-01-10 10:55:35 +0545

    add: react-helmet to display proper titles

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -9,9 +9,10 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useState } from "react"
+// import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
+import { Helmet } from "react-helmet"
 import * as z from "zod"
 
 const loginFormSchema = z.object({
@@ -39,69 +40,77 @@ const Login = () => {
   // }
 
   return (
-    <Card className="w-[350px] m-auto mt-36">
-      <CardHeader>
-        <CardTitle className="mx-auto">Login</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmitLoginForm)}
-            className="flex flex-col items-center gap-4"
-          >
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="email"
-                      placeholder="Enter your email address"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+    <>
+      <Helmet>
+        <title>Login - Task Manager</title>
+      </Helmet>
+      <Card className="w-[350px] m-auto mt-36">
+        <CardHeader>
+          <CardTitle className="mx-auto">Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmitLoginForm)}
+              className="flex flex-col items-center gap-4"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="Enter your email address"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem className="">
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="password"
-                      // type={passwordView ? "text" : "password"}
-                      placeholder="Enter your password"
-                      {...field}
-                    />
-                  </FormControl>
-                  {/* <p
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="">
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        // type={passwordView ? "text" : "password"}
+                        placeholder="Enter your password"
+                        {...field}
+                      />
+                    </FormControl>
+                    {/* <p
                     className="text-sm text-blue-500 cursor-pointer"
                     onClick={togglePasswordView}
                   >
                     {passwordView ? "Hide" : "Show"}
                   </p> */}
-                </FormItem>
-              )}
-            />
+                  </FormItem>
+                )}
+              />
 
-            <Button type="submit">Login</Button>
-          </form>
-        </Form>
+              <Button type="submit">Login</Button>
+            </form>
+          </Form>
 
-        <p className="text-center mt-2">
-          Don't have an account?{" "}
-          <Link to={"/register"} className="text-blue-500 hover:text-blue-700">
-            Register
-          </Link>
-        </p>
-      </CardContent>
-    </Card>
+          <p className="text-center mt-2">
+            Don't have an account?{" "}
+            <Link
+              to={"/register"}
+              className="text-blue-500 hover:text-blue-700"
+            >
+              Register
+            </Link>
+          </p>
+        </CardContent>
+      </Card>
+    </>
   )
 }
 
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -12,6 +12,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
 import { z } from "zod"
+import { Helmet } from "react-helmet"
 
 const registerFormSchema = z.object({
   email: z.string().min(3).max(50),
@@ -36,96 +37,101 @@ const Register = () => {
   }
 
   return (
-    <Card className="w-[350px] m-auto mt-36">
-      <CardHeader>
-        <CardTitle className="mx-auto">Register</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmitRegisterForm)}
-            className="flex flex-col items-center gap-4"
-          >
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="email"
-                      placeholder="Enter your email address"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+    <>
+      <Helmet>
+        <title>Register - Task Manager</title>
+      </Helmet>
+      <Card className="w-[350px] m-auto mt-36">
+        <CardHeader>
+          <CardTitle className="mx-auto">Register</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmitRegisterForm)}
+              className="flex flex-col items-center gap-4"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        placeholder="Enter your email address"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="text"
-                      placeholder="Enter your username"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        placeholder="Enter your username"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="password"
-                      placeholder="Enter a strong password"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Enter a strong password"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
 
-            <FormField
-              control={form.control}
-              name="confirm_password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Confirm Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="password"
-                      placeholder="Re-enter password to confirm"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+              <FormField
+                control={form.control}
+                name="confirm_password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Re-enter password to confirm"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
 
-            <Button type="submit">Register</Button>
-          </form>
-        </Form>
+              <Button type="submit">Register</Button>
+            </form>
+          </Form>
 
-        <p className="text-center mt-2">
-          Already have an account?{" "}
-          <Link to={"/"} className="text-blue-500 hover:text-blue-700">
-            Login
-          </Link>
-        </p>
-      </CardContent>
-    </Card>
+          <p className="text-center mt-2">
+            Already have an account?{" "}
+            <Link to={"/"} className="text-blue-500 hover:text-blue-700">
+              Login
+            </Link>
+          </p>
+        </CardContent>
+      </Card>
+    </>
   )
 }
 
