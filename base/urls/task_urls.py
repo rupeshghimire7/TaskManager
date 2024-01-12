895d4f56a18a0a1e80c5efa745f16a73b8e333f4
Rupesh Ghimire <rupeshghimire007@gmail.com>
2024-01-12 17:52:00 +0545

    Task Urls updated

--- base/urls/task_urls.py
+++ base/urls/task_urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from base.views import task_views as views
 
 
+
 urlpatterns = [
-    
+    path("",views.getTasks, name="tasks"),
+    path("create/", views.createTask, name='create_task'),
+    path("<str:pk>/", views.getTask, name='task'),
+    path("update/<str:pk>/", views.updateTask, name="update_task"),
+    path('delete/<str:pk>/', views.deleteTask, name="delete_task")
 ]
