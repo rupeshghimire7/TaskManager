084336e5afbbd9b828530f493d17a04587e44409
SandeshGC <gcsandesh01@gmail.com>
2024-02-15 19:57:54 +0545

    add: back button

--- frontend/src/pages/AddTask.tsx
+++ frontend/src/pages/AddTask.tsx
@@ -1,12 +1,24 @@
 import AddTaskForm from "@/components/AddTaskForm"
 import Layout from "@/components/Layout/Layout"
+import { Button } from "@/components/ui/button"
+import { MoveLeftIcon } from "lucide-react"
+import { Link } from "react-router-dom"
 
 const AddTask = () => {
   return (
     <Layout>
       <div>
-        <h1 className="text-center text-2xl font-bold my-2">Add Task</h1>
-
+        <div className="flex justify-between">
+          <Link to={"/"}>
+            <Button
+              className="bg-transparent text-gray-600 hover:bg-transparent    "
+              size={"icon"}
+            >
+              <MoveLeftIcon />
+            </Button>
+          </Link>
+          <h1 className="mx-auto text-2xl font-bold my-2">Add Task</h1>
+        </div>
         <AddTaskForm />
       </div>
     </Layout>
