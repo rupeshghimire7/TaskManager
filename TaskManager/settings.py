f8b01eaebe9fd46cfee2901a71eeef1daa5bdefb
Rupesh Ghimire <rupeshghimire007@gmail.com>
2024-02-15 21:48:58 +0545

    Function for getting is_completed of task

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -40,10 +40,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "base.apps.BaseConfig",
     "rest_framework",
     "rest_framework_simplejwt",
-    "corsheaders",
 ]
 
 
